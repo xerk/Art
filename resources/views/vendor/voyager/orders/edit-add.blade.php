fb458d82a0f8fcac75f9835d9b39d9b7401435e6
Ahmed Mamdouh <gm.xerk@gmail.com>
2018-03-22 04:05:59 +0200

    Printv0.1

---
+++ resources/views/vendor/voyager/orders/edit-add.blade.php
@@ -0,0 +1,14 @@
+@extends('voyager::master')
+@section('css')
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+@stop
+
+@section('page_title', __('voyager.generic.'.(!is_null($dataTypeContent->getKey()) ? 'edit' : 'add')).' '.$dataType->display_name_singular)
+
+@section('page_header')
+    <h1 class="page-title">
+        <i class="{{ $dataType->icon }}"></i>
+        {{ __('voyager.generic.'.(!is_null($dataTypeContent->getKey()) ? 'edit' : 'add')).' '.$dataType->display_name_singular }}
+    </h1>
+    @include('voyager::multilingual.language-selector')
+@stop
